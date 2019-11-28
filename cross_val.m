f100ab2915f0c4e8d8a27c3d4605d15525211655
Unknown <zhongqihua@gmail.com>
2019-11-28 02:28:55 -0500

    Added parameter Gamma for get_z()

--- cross_val.m
+++ cross_val.m
@@ -8,12 +8,15 @@ abar = [0.608 0.386 0.430 0.864]';
 ahat = [0.257 0.114 0.127 0.329]';
 cs = [8552.695 7195.047 6045.412 4381.518]';
 ds = [10000 5314.612 10000 10000]';
-sum_h = zeros(n,1)
-h_save = zeros(n,iter)
+sum_h = zeros(n,1);
+h_save = zeros(n,iter);
+
+Gamma = 1;
+
 %get the validation of samples
 for i = 1:iter
-    %z = get_z(n);
-    z = rand(n, 1);
+    z = get_z(n, Gamma);
+    %z = rand(n, 1);
     a = abar - ahat.* z;
     h_save(:,i) = cs.*(1+boxSol.x./ds).^a - cs;
     %h = cs.*(1+boxSol.x./ds).^a - cs;
@@ -24,10 +27,11 @@ sum_h = sum(h_save);
 hist(sum_h, 50);
 %boxplot(h_save');
 
+
 % define the uncertainty
-function z = get_z(n)
-x = rand(n, 1); % Start with 3 random numbers that don't sum to 1.
-z = x / sum(x)  % Normalize so the sum is 1.
+function z = get_z(n, Gamma)
+x = rand(n, 1); % Start with 4 random numbers that don't sum to Gamma.
+z = x * Gamma / sum(x);  % Normalize so the sum is Gamma.
 end
 
 
